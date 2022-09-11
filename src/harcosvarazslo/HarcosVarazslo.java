afb61c6f1c74caf6399ea6e4e94b54dad96b4a32
Norbus2000 <odornorbert.on@gmail.com>
2022-09-11 14:17:57 +0200

    kiindulas

---
+++ src/harcosvarazslo/HarcosVarazslo.java
@@ -0,0 +1,11 @@
+package harcosvarazslo;
+import nezet.Gui;
+
+public class HarcosVarazslo {
+
+    public static void main(String[] args) {
+        new Gui().setVisible(true);
+        
+    }
+    
+}
